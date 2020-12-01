cebcac347556d70bc5a9b886c72ac3cbeead570d
Ariane Jurado <arianejurado@FVFXCA8YHV29.local>
2020-12-01 19:33:55 +0100

    day 1 part 1 and 2

---
+++ template.js
@@ -0,0 +1,14 @@
+const inputText = ``
+
+function parseInput(inputText) {
+    return inputText
+}
+
+function resolve(input) {
+    return input
+}
+
+const input = parseInput(inputText)
+const output = resolve(input)
+
+console.log(output)
